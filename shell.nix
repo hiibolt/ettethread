6612e491a2cb93b4ac00af1a7e9f3a7b27e6d1f2
BoltR6 <johnwallacewhite@gmail.com>
2023-05-13 16:53:55 -0500

    Removed trailing characters (WSL issue)

--- shell.nix
+++ shell.nix
@@ -1,18 +1,17 @@
 { pkgs ? import <nixpkgs> {} }:
-let 
+let
 	node_packages = [ "openai" "twilio" "express" "socket.io" ];
 	get_node_modules = builtins.readDir ./node_modules;
 	check_package_exists = p: builtins.hasAttr p get_node_modules;
 	all_packages_exist = (builtins.pathExists ./node_modules) && (builtins.all (x: x == true) (map (p: check_package_exists p) node_packages));
-
 in
   pkgs.mkShell {
-    nativeBuildInputs = with pkgs; [ 
+    nativeBuildInputs = with pkgs; [
 		nodejs
     ];
-		shellHook = 
-			''
-			${if all_packages_exist then "echo \"All packages installed\"" else "npm install"}
-			./keys.sh
-			'';#
+	shellHook =
+		''
+		./keys.sh
+		${if all_packages_exist then "echo \"All packages installed\"" else "npm install"}
+		'';
 }
